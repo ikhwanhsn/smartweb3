051b4301c17879bdd19e8ebaad98178cc52f0224
ikhwanhsn <ikhwanulhusna111@gmail.com>
2024-01-22 07:20:14 +0700

    Chore: add button course

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import Button from "./components/Button"
-import ButtonForBusiness from "./components/ButtonForBusiness"
-import Footer from "./components/Footer"
-import ProfilDesc from "./components/ProfilDesc"
-import ProfilImage from "./components/ProfilImage"
-import Swal from 'sweetalert2'
-import {useNavigate} from 'react-router-dom'
-import ButtonBuyCrypto from "./components/ButtonBuyCrypto"
+import Button from "./components/Button";
+import ButtonForBusiness from "./components/ButtonForBusiness";
+import Footer from "./components/Footer";
+import ProfilDesc from "./components/ProfilDesc";
+import ProfilImage from "./components/ProfilImage";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import ButtonBuyCrypto from "./components/ButtonBuyCrypto";
+import ButtonCourse from "./components/ButtonCourse";
 
 function App() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen text-center ">
-      <ProfilImage/>
-      <ProfilDesc/>
-      <Button 
-      href={'https://t.me/SmartWeb3ID'} 
-      style={'w-40 md:w-64 px-5 py-3 mt-3 text-white bg-blue-500 rounded-full hover:bg-blue-600'}
-      >Telegram Group</Button>
+      <ProfilImage />
+      <ProfilDesc />
+      <Button
+        href={"https://t.me/SmartWeb3ID"}
+        style={
+          "w-40 md:w-64 px-5 py-3 mt-3 text-white bg-blue-500 rounded-full hover:bg-blue-600"
+        }
+      >
+        Telegram Group
+      </Button>
+      <ButtonCourse />
       {/* <button 
       onClick={() => Swal.fire(
         'Cooming Soon',
@@ -26,15 +32,17 @@ function App() {
       )} 
       className="w-40 px-5 py-3 mt-3 text-blue-500 border-2 border-blue-500 rounded-full hover:text-white hover:bg-blue-500"
       >📖Free Ebookk</button> */}
-      <ButtonBuyCrypto/>
-      <button 
-      onClick={() => navigate('/ebook') }
-      className="w-40 px-5 py-3 mt-3 text-blue-500 border-2 border-blue-500 rounded-full md:w-64 hover:text-white hover:bg-blue-500"
-      >📖Free Ebook</button>
-      <ButtonForBusiness/>
-      <Footer/>
+      <ButtonBuyCrypto />
+      <button
+        onClick={() => navigate("/ebook")}
+        className="w-40 px-5 py-3 mt-3 text-blue-500 border-2 border-blue-500 rounded-full md:w-64 hover:text-white hover:bg-blue-500"
+      >
+        📖Free Ebook
+      </button>
+      <ButtonForBusiness />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ButtonCourse.jsx
@@ -0,0 +1,47 @@
+import Button from "./Button";
+
+const ButtonCourse = () => {
+  return (
+    <section>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+        className="w-40 px-5 py-3 mt-3 text-blue-500 normal-case border-2 border-blue-500 rounded-full md:w-64 hover:text-white hover:bg-blue-500"
+      >
+        📚Course
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="text-lg font-bold">Mastering Dex</h3>
+          <section className="w-full mt-2 border-4 border-black rounded-md shadow-md">
+            <iframe
+              src="https://drive.google.com/file/d/1fihxMDb49b64n3ppMe8d1AKj2AegAY_A/preview"
+              width="640"
+              height="480"
+              allow="autoplay"
+              className="w-full h-64"
+            ></iframe>
+          </section>
+          <Button
+            href={"https://lynk.id/smartweb3.id/vv8X8wJ"}
+            style={
+              "w-40 md:w-64 px-5 py-3 mt-3 text-white bg-blue-500 rounded-full hover:bg-blue-600"
+            }
+          >
+            Buy Course{" "}
+            <span className="font-bold text-red-600 line-through">$50</span>{" "}
+            <span className="font-bold text-green-400">$37</span>
+          </Button>
+        </div>
+      </dialog>
+    </section>
+  );
+};
+
+export default ButtonCourse;
